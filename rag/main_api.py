95bf97fbd4b6e95e6947143a385a6d624db369d7
Hafiz Malik <malikhafiz397@gmail.com>
2025-12-28 12:52:55 +0500

    Update main_api.py

--- rag/main_api.py
+++ rag/main_api.py
@@ -1,32 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from qdrant_client import QdrantClient
-from sentence_transformers import SentenceTransformer
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from qdrant_client import QdrantClient
+# from sentence_transformers import SentenceTransformer
 
-app = FastAPI()
+# app = FastAPI()
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# model = SentenceTransformer("all-MiniLM-L6-v2")
 
-client = QdrantClient(
-    url="https://6bef06ec-6110-4599-9066-4872dde17237.europe-west3-0.gcp.cloud.qdrant.io:6333",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.O-yuuycAhIdMXTQ2DGDnBPOrknD6yOH4pwd0OlJjMYU",
-)
+# client = QdrantClient(
+#     url="https://6bef06ec-6110-4599-9066-4872dde17237.europe-west3-0.gcp.cloud.qdrant.io:6333",
+#     api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.O-yuuycAhIdMXTQ2DGDnBPOrknD6yOH4pwd0OlJjMYU",
+# )
 
-class Query(BaseModel):
-    question: str
+# class Query(BaseModel):
+#     question: str
 
-@app.post("/ask")
-def ask(query: Query):
-    vector = model.encode(query.question).tolist()
+# @app.post("/ask")
+# def ask(query: Query):
+#     vector = model.encode(query.question).tolist()
 
-    hits = client.search(
-        collection_name="book",
-        query_vector=vector,
-        limit=3
-    )
+#     hits = client.search(
+#         collection_name="book",
+#         query_vector=vector,
+#         limit=3
+#     )
 
-    context = "\n".join([hit.payload["text"] for hit in hits])
+#     context = "\n".join([hit.payload["text"] for hit in hits])
 
-    return {
-        "answer": context
-    }
+#     return {
+#         "answer": context
+#     }
